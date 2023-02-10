5b04a9fd45a7e22d1ffb26ebd67dd991189494a9
William Jorge <wjorgebe@purdue.edu>
2023-02-10 18:00:11 -0500

    Use github token, remove Talk

--- 461_CLI/github-api.ts
+++ 461_CLI/github-api.ts
@@ -2,18 +2,30 @@ import axios from 'axios';
 
 const API_BASE_URL = 'https://api.github.com';
 
-async function getRepository(username: string, repoName: string, token: string): Promise<any> {
+interface GitHubUser {
+  login: string;
+  id: number;
+  avatar_url: string;
+}
+
+async function getUser(username: string): Promise<GitHubUser> {
   try {
-    const response = await axios.get(`${API_BASE_URL}/repos/${username}/${repoName}`, {
+    const response = await axios.get(`${API_BASE_URL}/users/${username}`, {
       headers: {
-        Authorization: `Token ${token}`,
+        Authorization: `Token ${process.env.GITHUB_TOKEN}`,
       },
     });
     return response.data;
   } catch (error) {
     console.error(error);
-    return null;
+    throw error;
   }
 }
 
-export default { getRepository };
+
+async function main() {
+  const user = await getUser('octocat');
+  console.log(user);
+}
+
+main();
