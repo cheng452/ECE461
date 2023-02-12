b2cc2a25a6cec60e5359ca46dab4cf6667c62c9c
William Jorge <wjorgebe@purdue.edu>
2023-02-11 20:02:48 -0500

    Current slay, graphql works yasss

---
+++ 461_CLI/src/contributors.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import { environment } from '../environment/environment';
+import {request} from "graphql-request";
+
+const args = process.argv.slice(2)
+const fs = require('fs');
+
+async function getNumContributors(owner, repo) {
+    const response = await axios.get(`https://api.github.com/repos/${owner}/${repo}/contributors`, {
+        headers: {
+            'Authorization': `Token ${environment.GITHUB_TOKEN}`
+        }
+    });
+    console.log(response.data.length);
+    // return response.data.length;
+  }
+  
+  getNumContributors(args[0], args[1]);
--- 461_CLI/src/graphql.ts
+++ 461_CLI/src/graphql.ts
@@ -1,65 +1,183 @@
-import axios from 'axios';
+// import axios from 'axios';
 import { environment } from '../environment/environment';
+// import {request} from "graphql-request";
 
-const API_URI = 'https://api.github.com/graphql';
+// const endpoint = 'https://api.github.com/graphql';
 const args = process.argv.slice(2)
 const fs = require('fs');
 
-//size, lang = ramp up
-//watchers versus # of forks = 
-//open issues
-//popularity 
+// // //size, lang = ramp up
+// // //watchers versus # of forks = 
+// // //open issues
+// // //popularity 
 
-const GET_REPOSITORY = `
-  query($repoName: String!, $owner: String!) {
-    repository(name: $repoName, owner: $owner) {
-      size
+// // // const query = `
+// // // query {
+// // //   repository(owner: "{owner}", name: "{repo}") {
+// // //     contributors(first: 100) {
+// // //       totalCount
+// // //     }
+// // //   }
+// // // }
+// // // `;
+
+// // // async function getNumContributors(owner: string, repo: string) {
+// // //   const result = await axios.post("https://api.github.com/graphql", {
+// // //     query: query.replace("{owner}", owner).replace("{repo}", repo),
+// // //     headers: {
+// // //       Authorization: `Bearer ${environment.GITHUB_BASIC_TOKEN}`
+// // //     }
+// // //   });
+
+// // //   console.log(result.data.data.repository.contributors.totalCount)
+// // //   return result.data.data.repository.contributors.totalCount;
+// // // }
+
+// // const query = `
+// // query {
+// //   repository(owner: "{owner}", name: "{repo}") {
+// //     contributors(first: 100) {
+// //       nodes {
+// //         name
+// //         email
+// //         contributions
+// //       }
+// //     }
+// //   }
+// // }
+// // `;
+
+// const query = `
+// query {
+//   repository(owner: "{owner}", name: "{repo}") {
+//     issues(states: OPEN) {
+//       totalCount
+//     }
+//     forks {
+//       totalCount
+//     }
+//     subscribers {
+//       totalCount
+//     }
+//   }
+// }
+// `;
+
+// interface RepositoryData {
+//   issues: {
+//     totalCount: number;
+//   };
+//   forks: {
+//     totalCount: number;
+//   };
+//   subscribers: {
+//     totalCount: number;
+//   };
+// }
+
+// async function getRepositoryData(owner: string, repo: string): Promise<RepositoryData> {
+//   const result = await axios.post("https://api.github.com/graphql", {
+//     query: query.replace("{owner}", owner).replace("{repo}", repo),
+//     headers: {
+//       "Authorization": `Bearer ${environment.GITHUB_BASIC_TOKEN}`,
+//        "Content-Type": "application/json"
+//     }
+//   });
+
+//   return result.data.data.repository;
+// }
+
+// getRepositoryData('cloudinary', 'cloudinary_npm')
+
+// // const query = `
+// //   query {
+// //     repository(owner: "owner", name: "repo-name") {
+// //       contributors(first: 100) {
+// //         nodes {
+// //           name
+// //           email
+// //           contributions
+// //         }
+// //       }
+// //     }
+// //   }
+// // `;
+
+// // const endpoint = "https://api.github.com/graphql";
+
+// // const headers = {
+// //   "Authorization": `Bearer ${environment.GITHUB_BASIC_TOKEN}`,
+// //   "Content-Type": "application/json"
+// // };
+
+// // const data = {
+// //   query: query
+// // };
+
+// // axios.post(endpoint, data, { headers: headers })
+// //   .then(response => {
+// //     const contributors = response.data.data.repository.contributors.nodes;
+// //     const busFactor = contributors.reduce((acc, contributor) => {
+// //       return acc + (contributor.contributions >= 10 ? 1 : 0);
+// //     }, 0);
+// //     console.log(`The bus factor for this repository is ${busFactor}.`);
+// //   })
+// //   .catch(error => {
+// //     console.error(error);
+// //   });
+
+// // async function getNumContributors(owner, repo) {
+// //   const response = await axios.get(`https://api.github.com/repos/${owner}/${repo}/contributors`);
+// //   console.log(response.data.length);
+// //   return response.data.length;
+// // }
+
+// // getNumContributors('cloudinary', 'cloudinary_npm')
+
+
+
+import axios from "axios";
+
+interface SubscriberData {
+  totalCount: number;
+}
+
+interface RepositoryData {
+  stargazers: SubscriberData;
+}
+
+
+const query = `
+  query {
+    repository(owner: "{owner}", name: "{repo}") {
+      forkCount
+      stargazers {
+        totalCount
+      }
+      issues(first: 100, states:OPEN) {
+        totalCount
+      }
     }
   }
 `;
 
-interface Repository {
-  name: string;
-  url: string;
-  description: string;
-  createdAt: string;
-  updatedAt: string;
-}
+const headers = {
+  Authorization: `Bearer ${environment.GITHUB_BASIC_TOKEN}`,
+};
 
-interface ResponseData {
-  data: {
-    repository: Repository;
-  };
-}
-
-async function getRepository(
-    repoName: string,
-    owner: string
-  ): Promise<Repository> {
-    try {
-      const response = await axios.post<ResponseData>(API_URI, {
-        query: GET_REPOSITORY,
-        variables: {
-          repoName,
-          owner,
-        },
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${environment.GITHUB_BASIC_TOKEN}`,
-        },
-      });
-  
-      return response.data.data.repository;
-    } catch (error) {
-      console.error(error);
-      throw error;
-    }
-}
-
-async function main() {
-  const repository = await getRepository(args[1], args[0]);
-  console.log(repository);
-}
-
-main();
+axios
+  .post("https://api.github.com/graphql", { query: query.replace("{owner}", args[0]).replace("{repo}", args[1]),}, { headers })
+  .then((result) => {
+    // fs.writeFileSync('out/' + args[1] + '_GRAPHQL.json', JSON.stringify(result, null, 2));
+    // console.log(result.data);
+    const forkies = result.data.data.repository.forkCount;
+    // console.log(`Forks: ${forkies}.`);
+    const subscribers = result.data.data.repository.stargazers.totalCount;
+    // console.log(`Subscribers: ${subscribers}.`);
+    const issues_out = result.data.data.repository.issues.totalCount;
+    // console.log(`Number of issues: ${issues_out}.`);
+    console.log(forkies + ", " + subscribers + ", " + issues_out)
+  })
+  .catch((error) => {
+    console.error(error);
+  });
